a456111e84494afb7c53d6f7dc6fe0431df8ad84
Halimjon Juraev <hjuraev@nexustls.com>
2019-06-12 10:32:11 +0500

    fixed package manager dependency mistake

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
 
     ],
     dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0")
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.0")
